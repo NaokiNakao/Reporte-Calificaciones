2ee83cff1970bd6a27966023f194b367254b322a
Naoki Nakao <naokinakao@gmail.com>
2021-02-26 09:34:24 -0500

    Funcion getCalifEval

--- main.c
+++ main.c
@@ -8,11 +8,15 @@
 #define LENNOMBRE   15
 #define LENAPEL     15
 #define LENDESC     15
-#define MAXCLAVE     8
+#define MAXCLAVE    20
 #define MAXEST       6
 
 #define MIN      0
 #define MAX    100
+#define P1    0.20
+#define P2    0.30
+#define EF    0.25
+#define TA    0.25
 
 /* Estructuras */
 
@@ -35,6 +39,7 @@ typedef struct{
 /* Prototipos de función */
 
 GRUPO getGrupo(void);
+void getCalifEval(EVAL*, char*, char*, char*);
 void setColor(int, int);
 void defaultColor();
 
@@ -81,25 +86,10 @@ GRUPO getGrupo(void)
       fflush(stdin);
       gets(gr.est[index].papellido);
 
-      do {
-         printf("Parcial 1: ");
-         scanf("%f", &gr.p1[index].valor);
-      } while (gr.p1[index].valor < MIN || gr.p1[index].valor > MAX);
-
-      do {
-         printf("Parcial 2: ");
-         scanf("%f", &gr.p2[index].valor);
-      } while (gr.p2[index].valor < MIN || gr.p2[index].valor > MAX);
-
-      do {
-         printf("Examen final: ");
-         scanf("%f", &gr.ef[index].valor);
-      } while (gr.ef[index].valor < MIN || gr.ef[index].valor > MAX);
-
-      do {
-         printf("Tareas: ");
-         scanf("%f", &gr.ta[index].valor);
-      } while (gr.ta[index].valor < MIN || gr.ta[index].valor > MAX);
+      getCalifEval(&gr.p1[index], "Parcial 1", "Procentaje del parcial 1", "Descripcion del parcial 1");
+      getCalifEval(&gr.p2[index], "Parcial 2", "Procentaje del parcial 2", "Descripcion del parcial 2");
+      getCalifEval(&gr.ef[index], "Examen final", "Procentaje del examen final", "Descripcion del examen final");
+      getCalifEval(&gr.ta[index], "Tareas", "Procentaje de las tareas", "Descripcion de las tareas");
 
       printf("\n");
    }
@@ -129,6 +119,34 @@ void setColor(int ct, int cf)
 void defaultColor()
 {
    setColor(WHITE, BLACK);
+}
+
+/*
+   Función    : getCalifEval
+   Argumentos : EVAL* eval: estructura con las evaluaciones
+                char* str1: mensaje del tipo de evaluación
+                char* str2: mensaje del porcentaje
+                char* str3: mensaje de descripción
+   Objetivo   : capturar los diferentes parámetros de las evaluaciones
+   Retorno    : ---
+*/
+void getCalifEval(EVAL* eval, char* str1, char* str2, char* str3)
+{
+   do {
+      printf("%s: ", str1);
+      scanf("%f", &eval->valor);
+   } while (eval->valor < MIN || eval->valor > MAX);
+
+   do {
+      printf("%s: ", str2);
+      scanf("%f", &eval->porc);
+   } while (eval->porc < 0 || eval->porc > 1);
+
+   printf("%s: ", str3);
+   fflush(stdin);
+   gets(eval->desc);
+
+   return;
 }
 
 
