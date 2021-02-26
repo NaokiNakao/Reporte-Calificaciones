e3225a2d0b91ad9597ad129d3835573fc727c031
Naoki Nakao <naokinakao@gmail.com>
2021-02-26 12:05:59 -0500

    Menu de opciones

--- main.c
+++ main.c
@@ -19,12 +19,22 @@
 
 /* Macros */
 
+#define TRUE    1
+#define FALSE   0
+
 #define MAXID        8
 #define LENNOMBRE   15
 #define LENAPEL     15
 #define LENDESC     15
 #define MAXCLAVE    20
 #define MAXEST       6
+#define MAXOPC       7
+#define CHAROPC     40
+
+#define UP      72
+#define DOWN    80
+#define ESC     27
+#define ENTER   13
 
 #define MIN      0
 #define MAX    100
@@ -53,16 +63,23 @@ typedef struct{
 GRUPO getGrupo(void);
 void setColor(int, int);
 void defaultColor();
+void selectOpciones(GRUPO*);
+void showOpciones(char [][CHAROPC], int);
 void getCalifEval(EVAL*, char*, char*, char*);
-void calcPP(GRUPO*);
+float calcCalif(GRUPO*, int);
 void notaMinMax(EVAL*, int*, int*);
 
 int main()
 {
    GRUPO grupo;
+   int est;
 
-   grupo = getGrupo();
-   calcPP(&grupo);
+   //grupo = getGrupo();
+   selectOpciones(&grupo);
+
+   // calculando el promedio parcial
+   //for (est = 0; est < MAXEST; est++)
+      //grupo.pp[est].valor = (calcCalif(&grupo, est) - grupo.ef[est].valor*grupo.ef[est].porc) / PP;
 
    return 0;
 }
@@ -137,6 +154,80 @@ void defaultColor()
 }
 
 /*
+   Función    : selectOpciones
+   Argumentos : GRUPO* gr: estructura con la información del grupo
+   Objetivo   : presentar menú de opciones para elegir
+   Retorno    : ---
+*/
+void selectOpciones(GRUPO* gr)
+{
+   char key, index = 0;
+
+   char opciones[][CHAROPC] = {"Nota maxima y minima por evaluacion",
+                               "Mayor nota sobre el promedio en P1 ",
+                               "Mayor nota sobre el promedio en P2 ",
+                               "Mayor nota sobre el promedio en EF ",
+                               "Mayor nota sobre el promedio en TA ",
+                               "Posibilidades de nota              ",
+                               "Calificaciones finales             "};
+
+   _setcursortype(FALSE);
+
+   gotoxy(3,3);
+   setColor(WHITE, LIGHTBLUE);
+   printf("OPCIONES:");
+   defaultColor();
+
+   do {
+
+      showOpciones(opciones, index);
+
+      do {
+         key = getch();
+      } while (key != UP && key != DOWN && key != ENTER && key != ESC);
+
+      if (key == UP)
+      {
+         if (index)
+            index--;
+         else
+            index = MAXOPC-1;
+      }
+      if (key == DOWN)
+      {
+         if (index < MAXOPC-1)
+            index++;
+         else
+            index = 0;
+      }
+
+   } while (key != ENTER && key != ESC);
+}
+
+/*
+   Función    : showOpciones
+   Argumentos : char opc[][CHAROPC]: contiene la descripción de las opciones
+                int select: posición de la opción seleccionada actualmente
+   Objetivo   : mostrar las opciones
+   Retorno    : ---
+*/
+void showOpciones(char opc[][CHAROPC], int select)
+{
+   int index, pos_x = 3, pos_y = 5;
+
+   for (index = 0; index < MAXOPC; index++)
+   {
+      gotoxy(pos_x, pos_y+index);
+      if (index == select)
+         setColor(BLUE, YELLOW);
+      printf("%s", opc[index]);
+      defaultColor();
+   }
+
+   return;
+}
+
+/*
    Función    : getCalifEval
    Argumentos : EVAL* eval: estructura con las evaluaciones
                 char* str1: mensaje del tipo de evaluación
@@ -165,22 +256,18 @@ void getCalifEval(EVAL* eval, char* str1, char* str2, char* str3)
 }
 
 /*
-   Función    : calcPP
+   Función    : calcCalif
    Argumentos : GRUPO* gr: estructura con la información del grupo
-   Objetivo   : calcular el promedio parcial en base a 75% de los estudiantes
-   Retorno    : ---
+                int est: índice del estudiante
+   Objetivo   : calcular las calificaciones de los estudiantes
+   Retorno    : (float) result: calificación total del estudiante
 */
-void calcPP(GRUPO* gr)
+float calcCalif(GRUPO* gr, int est)
 {
-   int index;
-
-   for (index = 0; index < MAXEST; index++)
-   {
-      gr->pp->valor = ( (gr->p1->valor*gr->p1->porc) + (gr->p2->valor*gr->p2->porc) +
-                       (gr->ef->valor*gr->ef->porc) + (gr->ta->valor*gr->ta->porc) ) / PP;
-   }
-
-   return;
+   float result;
+   result = (gr->p1[est].valor*gr->p1[est].porc) + (gr->p2[est].valor*gr->p2[est].porc) +
+            (gr->ef[est].valor*gr->ef[est].porc) + (gr->ta[est].valor*gr->ta[est].porc);
+   return result;
 }
 
 /*
